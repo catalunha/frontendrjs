1459722398b4bd0ae4ca505232663ead639bc235
catalunha <catalunha.mj@gmail.com>
2020-05-09 14:54:16 -0300

    projeto terminado

--- src/pages/Incident/New/index.js
+++ src/pages/Incident/New/index.js
@@ -1,9 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import './styles.css'
 import logoImg from '../../../assets/logo.svg'
+import api from '../../../services/api'
 export default function IncidentNew() {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [value, setValue] = useState('')
+  const ongId = localStorage.getItem('ongId')
+  const history = useHistory()
+  async function handleIncidentNew(e) {
+    e.preventDefault()
+    const data = {
+      title,
+      description,
+      value,
+    }
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId
+        }
+      })
+      history.push('/profile')
+    } catch (error) {
+      alert('Erro ao cadastrar caso' + error)
+    }
+  }
   return (
     <div className="incidentnew-container">
       <div className="content">
@@ -16,10 +40,10 @@ export default function IncidentNew() {
             Voltar para home
             </Link>
         </section>
-        <form action="">
-          <input type="text" placeholder='Titulo do caso' />
-          <textarea placeholder='Descrição' />
-          <input type="text" placeholder='Valor em R$' />
+        <form onSubmit={handleIncidentNew}>
+          <input type="text" value={title} onChange={e => setTitle(e.target.value)} placeholder='Titulo do caso' />
+          <textarea value={description} onChange={e => setDescription(e.target.value)} placeholder='Descrição' />
+          <input type="text" value={value} onChange={e => setValue(e.target.value)} placeholder='Valor em R$' />
 
           <button className="button" type='submit'>Cadastrar</button>
         </form>
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,22 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link,useHistory } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi'
 import './styles.css'
 import heroesImg from '../../assets/heroes.png'
 import logoImg from '../../assets/logo.svg'
+import api from '../../services/api'
 export default function Login() {
+  const [id, setId] = useState('')
+  const history = useHistory()
+  async function handleLogin(e) {
+    const data = { id }
+    e.preventDefault()
+    try {
+      const response = await api.post('sessions', data)
+      localStorage.setItem('ongId',id)
+      localStorage.setItem('ongName',response.data.name)
+      console.log(response.data.name)
+      history.push('/profile')
+    } catch (error) {
+      alert('Falha no login.')
+    }
+  }
   return (
     <div className="login-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
-        <form action="">
+        <form onSubmit={handleLogin}>
           <h1>Faça seu login</h1>
-          <input placeholder='Sua identificação'></input>
+          <input placeholder='Sua identificação' value={id} onChange={e => setId(e.target.value)} ></input>
           <button className='button' type='submit'>Entrar</button>
           <Link to='/register' className='back-link'>
             <FiLogIn size={16} color="#e02041" />
             Não tenho cadastro
-            </Link>
+          </Link>
         </form>
       </section>
       <img src={heroesImg} alt="Heroes" />
